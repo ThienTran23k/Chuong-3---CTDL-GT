7636ecc5f08d092693c1297e250bd05a9def841a
ThienTranK32 <125123982+ThienTran23k@users.noreply.github.com>
2023-03-06 19:40:13 +0700

    Add files via upload

---
+++ Sap Xep giam dan.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#define MAX 50
+ using namespace std;
+ 
+int NhapsoPTsudung ()
+{
+	int n ;
+	do{
+		cout << " Nhap so phan tu su dung : " ;
+		cin >> n ;
+	}while (n < 1 || n > MAX) ;
+	return n ;
+}
+
+void  NhapMang(int a[MAX],int n)
+{
+	for (int i =0 ; i < n ; i ++)
+		{
+			cout << "a[" << i << "] = "; 
+			cin >> a[i] ; 
+		}
+}
+
+ int SapXepGiamDan(int a[], int n)
+{
+ for(int i = 0; i < n - 1; i++)
+ 	{
+        for(int j = i + 1; j < n; j++)
+			{
+            if(a[i] < a[j])
+				{
+                // Hoan vi 2 so a[i] va a[j]
+                int tg = a[i];
+                a[i] = a[j];
+                a[j] = tg;        
+            	}
+        }
+    }
+}
+
+void XuatMang(int a [MAX],int n)
+{
+	cout << "Mang sau khi sap xep : " << endl ;
+	for (int i = 0 ; i < n; i++)
+	{
+		cout << "a[" << i << "] = " << a[i] <<endl ;
+	}	
+}
+
+int main (){
+	int a[MAX], n ;
+	n = NhapsoPTsudung() ;
+	NhapMang(a,n);
+	SapXepGiamDan(a,n);
+	XuatMang(a,n);
+	return 0 ;
+	
+}
